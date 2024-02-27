e8342d1bcf00d26821d3d38f9ecf30f15e02e254
solvei9 <solvei9@yandex.ru>
2024-02-27 19:12:46 +0700

    Add class, service methods and test for creating contacts

---
+++ contact.py
@@ -0,0 +1,14 @@
+class Contact:
+    def __init__(self, firstname, lastname, nickname, title, company, address, mobile, email, bday, bmonth,
+                      byear):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
