41a1d5571e04673e1ca2c9b6bee9da6bc3a15247
solvei9 <solvei9@yandex.ru>
2024-03-03 16:40:07 +0700

    Fix misprints

--- fixture/group.py
+++ fixture/group.py
@@ -26,8 +26,8 @@ class GroupHelper:
         wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element(By.NAME, "submit").click()
-        self.app.return_to_groups_page()
+        self.app.group.return_to_groups_page()
 
     def return_to_groups_page(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element(By.LINK_TEXT, "groups").click()
