bb444d33f524a74d538534d0e3532f5103e0ae6d
solvei9 <solvei9@yandex.ru>
2024-03-10 18:30:35 +0700

    Add checks to login and logout methods

--- fixture/session.py
+++ fixture/session.py
@@ -1,4 +1,5 @@
 import time
+
 from selenium.webdriver.common.by import By
 
 
@@ -18,7 +19,27 @@ class SessionHelper:
         wd.find_element(By.NAME, "pass").send_keys(password)
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
+    def ensure_login(self, username, password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
     def logout(self):
         wd = self.app.wd
         wd.find_element(By.LINK_TEXT, "Logout").click()
         time.sleep(3)
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element(By.XPATH, "//div/div[1]/form/b").text == "(" + username + ")"
