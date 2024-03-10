f323ddd58ec94c1e8a37a0c959d8b3b40695ac2a
solvei9 <solvei9@yandex.ru>
2024-03-10 19:08:48 +0700

    Optimize transitions between pages

--- fixture/application.py
+++ fixture/application.py
@@ -9,7 +9,6 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -26,4 +25,5 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        if not wd.current_url.endswith("localhost/addressbook/"):
+            wd.get("http://localhost/addressbook/")
--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element(By.LINK_TEXT, "groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements(By.NAME, "new")) > 0):
+            wd.find_element(By.LINK_TEXT, "groups").click()
 
     def create(self, group):
         wd = self.app.wd
