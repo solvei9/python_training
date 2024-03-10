d8220fcd81c595eaaf8561374965fc54038a2ddc
solvei9 <solvei9@yandex.ru>
2024-03-10 10:42:28 +0700

    Add tests for modifying group name and group header

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,3 +5,15 @@ def test_modify_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="New group", header="New group header", footer="New group footer"))
     app.session.logout()
+
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New group 1"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New group header 1"))
+    app.session.logout()
