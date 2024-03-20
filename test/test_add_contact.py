9f03054e009dc2f20b1d99956faf2969bc7766e9
solvei9 <solvei9@yandex.ru>
2024-03-20 13:46:38 +0700

    Add hash for group list and contact list

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -8,8 +8,8 @@ def test_add_contact(app):
                                company="DevCompany", address="Moscow", mobile="+7 933 933 4545", email="petrov@mail.ru",
                                bday="5", bmonth="August", byear="1976")
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
@@ -19,7 +19,7 @@ def test_add_empty_contact(app):
     contact = Contact(firstname="", lastname="", nickname="", title="", company="", address="", mobile="",
                                email="", bday="", bmonth="", byear="")
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
