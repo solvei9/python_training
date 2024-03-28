a983d2622b5a3d93f7bec52def50914c47e5ed4d
solvei9 <solvei9@yandex.ru>
2024-03-28 14:23:30 +0700

    Add command line options

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 import pytest
-
 from fixture.application import Application
 
 fixture = None
@@ -8,12 +7,16 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+    username = request.config.getoption("--username")
+    password = request.config.getoption("--password")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
+    fixture.session.ensure_login(username=username, password=password)
     return fixture
 
 
@@ -25,3 +28,10 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--username", action="store", default="admin")
+    parser.addoption("--password", action="store", default="secret")
--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
-
+from selenium import webdriver
 from fixture.contact import ContactHelper
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
@@ -7,11 +6,19 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s", browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -26,4 +33,4 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not wd.current_url.endswith("localhost/addressbook/"):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
