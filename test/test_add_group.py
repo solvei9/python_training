49f00b0ec1bc2ee58d2885497fd6a874510cf052
solvei9 <solvei9@yandex.ru>
2024-03-31 10:57:30 +0700

    Add package for test data

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.group import Group
-from model.general import random_string
-
-
-testdata = [
-    Group(name=name, header=header, footer=footer)
-    for name in ["", random_string("name", 10)]
-    for header in ["", random_string("header", 20)]
-    for footer in ["", random_string("footer", 20)]
-]
+from data.add_group import testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
