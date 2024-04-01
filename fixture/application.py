ce24251e2162fb7ef9c9c161a859a71607172a4f
solvei9 <solvei9@yandex.ru>
2024-04-01 16:32:06 +0700

    Remove application url from code

--- fixture/application.py
+++ fixture/application.py
@@ -32,5 +32,5 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not wd.current_url.endswith("localhost/addressbook/"):
+        if not wd.current_url.endswith(self.base_url):
             wd.get(self.base_url)
